618b9b4bfa3ec7c36ab71836f9107e64b456c3e9
Matteturtle09 <mauri.matteo16@gmail.com>
2022-11-05 16:43:11 +0000

    added cors and some enanchements

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const app = require("express")();
 const {createHash} = require("crypto");
-require('dotenv').config()
+require('dotenv').config();
+
 const PORT = 8080;
 
-var mqtt = require('mqtt')
+var mqtt = require('mqtt');
+var cors = require('cors');
+
+
+
+
 
 var options = {
     host: '21a62d44d56a4ded8e53696c6ee7025a.s1.eu.hivemq.cloud',
     port: 8883,
     protocol: 'mqtts',
     username: 'CRBapp',
-    password: process.env.password
+    password: process.env.PASSWORD
 }
 
 // initialize the MQTT client
@@ -18,31 +24,47 @@ var client = mqtt.connect(options);
 
 app.listen(PORT, () => console.log("inizio ora"));
 
+app.use(cors({
+  origin: 'https://crbpwa.netlify.app',
+}));
+
+app.set('trust proxy', true);
+
 app.get("/api/status", (req, res) => {
   res.status(200).send({
     api: "on",
     mouse: "micky",
+    ip: req.ip,
   });
 });
 
 
 
-app.post("/api/mqtt/:id/:message/:apikiwi", (req, res) => {
+app.get("/api/mqtt/:id/:message/:apikiwi", (req, res) => {
   const { id } = req.params;
   const { message } = req.params;
   const { apikiwi } = req.params;
 
-  if (apikiwi === process.env.apikey) {
-    client.publish(id, message);
-    res.status(200).send({
-      channel: createHash("sha256").update(id).digest('hex'),
-      message: createHash("sha256").update(message).digest('hex')
+  if (apikiwi === process.env.APIKEY) {
+    client.publish(id, message, function (error) {
+      if (error) {
+        console.log(error)
+      } else {
+        res.status(200).send({
+          channel: createHash("sha256").update(id).digest('hex'),
+          message: createHash("sha256").update(message).digest('hex')
+        });
+      }
     });
+    
+    
   } else {
     res.status(401).send({
       channel: id,
       message: message,
-      errore: "🔴"
+      errore: "🔴",
+          
     });
+    
   }
 });
